687c53f9ea22098e5dcf9391d5879575f6dde4b2
Denys-Dev <mrdenys18@gmail.com>
2022-06-26 17:28:32 -0300

    desafio 2 finalizado

---
+++ script.js
@@ -0,0 +1,26 @@
+let idade =19
+
+if(idade >= 10){
+    console.log( "voce é maior de idade");
+}
+else {
+    console.log("voce é de menor de idade");
+}
+
+let humano = false
+
+if(humano == true && idade >= 18){
+    console.log("voce é humano");
+}
+else{
+    console.log("voce nao é humano");
+}
+
+let mes = "fevereiro"
+
+if( mes == "janeiro" || mes == "Dezembro"){
+    console.log("feliz aniversario");
+}
+else{
+    console.log("Nao e seu niver");
+}
